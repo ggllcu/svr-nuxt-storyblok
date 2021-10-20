a033e280710b4de1d66e81e9c183485271dbaeec
Luca Guglielmi <luca.guglielmi@intesys.it [push]>
2021-10-20 02:08:50 +0200

    Variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,9 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
-  target: 'static',
+  target: process.env.TARGET,
+  ssr: process.env.SSR
+
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
