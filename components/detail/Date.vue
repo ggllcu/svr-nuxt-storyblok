2f1e39a7d0fceba978104689d1c3d1cff7bf7dfd
Luca Guglielmi <luca.guglielmi@intesys.it [push]>
2021-11-03 02:02:13 +0100

    About Page

---
+++ components/detail/Date.vue
@@ -0,0 +1,35 @@
+<template>
+  <time>
+    <p class="has-text-dark">
+      {{ niceDate }}
+    </p>
+  </time>
+</template>
+
+<style lang="scss" scoped>
+time p {
+  margin-bottom: 1.5rem;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: String,
+      required: true,
+      default: null
+    }
+  },
+  computed: {
+    niceDate () {
+      // console.log('this.date', this.date)
+      const dateObj = new Date(this.date)
+      const day = dateObj.getDate()
+      const month = dateObj.toLocaleString('it-IT', { month: 'long' }).replace(/^\w/, c => c.toUpperCase())
+      const year = dateObj.getFullYear()
+      return day + ' ' + month + ' ' + year
+    }
+  }
+}
+</script>
---
+++ components/detail/Figure.vue
@@ -0,0 +1,20 @@
+<template>
+  <figure class="image is-2by1">
+    <img :src="image">
+  </figure>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
